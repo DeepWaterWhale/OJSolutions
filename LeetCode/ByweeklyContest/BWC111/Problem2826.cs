f457cbec9b5b1ca21cd8f58c0819c2270e79c217
xiangjli <xiangjli@microsoft.com>
2023-09-23 12:03:15 +0800

    Update Problem2826

---
+++ LeetCode/ByweeklyContest/BWC111/Problem2826.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.ByweeklyContest.BWC111
+{
+    internal class Problem2826
+    {
+        public class Solution
+        {
+            public int MinimumOperations(IList<int> nums)
+            {
+                int ans = nums.Count();
+
+                for (int i = -1; i <= nums.Count; i++)
+                {
+                    for (int j = i; j <= nums.Count; j++)
+                    {
+                        // i is the first index of 2, j can be -1 means all elements are 3
+                        // j is the first index of 3, j can be nums.Count means no elements is 3
+                        ans = Math.Min(ans, this.Operations(nums, i, j));
+                    }
+                }
+
+                return ans;
+            }
+
+            private int Operations(IList<int> nums, int i2, int i3)
+            {
+                int index = 0;
+                int cnt = 0;
+                while (index < nums.Count)
+                {
+                    if (index < i2 && nums[index] != 1)
+                    {
+                        cnt++;
+                    }
+
+                    if (i2 <= index && index < i3 && nums[index] != 2)
+                    {
+                        cnt++;
+                    }
+
+                    if (i3 <= index && nums[index] != 3)
+                    {
+                        cnt++;
+                    }
+
+                    index++;
+                }
+
+                return cnt;
+            }
+        }
+    }
+}
---
+++ LeetCode/ByweeklyContest/BWC111/Problem2827.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.ByweeklyContest.BWC111
+{
+    internal class Problem2827
+    {
+        public class Solution
+        {
+            public int NumberOfBeautifulIntegers(int low, int high, int k)
+            {
+
+            }
+
+            // The number of integers with <k> digits and digits sum is target
+            private int NumberOfCombination(int target, int digitCounts)
+            {
+
+            }
+        }
+    }
+}
