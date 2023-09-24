5d329d017ea3f0103b4e307c8f0a22dd792c1f36
xiangjli <xiangjun.app@hotmail.com>
2023-09-24 11:16:05 +0800

    Solve problem 0233

---
+++ LeetCode/RandomPick/Problem0233.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.TODOs
+{
+    using Shared.DynamicProgramming;
+
+    internal class Problem0233
+    {
+        public class Solution
+        {
+            public int CountDigitOne(int n)
+            {
+                var ddp = new DigitDp<int>(
+                    right: n,
+                    statusUpdateFunc: (count1, digit) =>
+                    {
+                        if (digit == 1)
+                        {
+                            return count1 + 1;
+                        }
+
+                        return count1;
+                    },
+                    endingFunc: count1 =>
+                    {
+                        return count1;
+                    });
+
+                return ddp.Solve(n.ToString().Length - 1, DigitDpPrefixStatus.LeadingZeros, 0);
+            }
+        }
+    }
+}
