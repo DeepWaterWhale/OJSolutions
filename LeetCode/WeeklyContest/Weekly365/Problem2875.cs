6cac7208ae5eb05cf52796d5236f696267b93eef
xiangjli <xiangjun.app@hotmail.com>
2023-10-06 20:40:45 +0800

    Update Weekly365

---
+++ LeetCode/WeeklyContest/Weekly365/Problem2875.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCode.WeeklyContest.Weekly365
+{
+    internal class Problem2875
+    {
+        public class Solution
+        {
+            public int MinSizeSubarray(int[] nums, int target)
+            {
+                long[] leftSums = new long[nums.Length * 2];
+                long tmp = 0;
+                for (int i = 0; i < nums.Length; ++i)
+                {
+                    tmp += nums[i];
+                    leftSums[i] = tmp;
+                }
+
+                for (int i = nums.Length; i < leftSums.Length; ++i)
+                {
+                    tmp += nums[i - nums.Length];
+                    leftSums[i] = tmp;
+                }
+
+                int ans = 0;
+                if (target >= leftSums[nums.Length - 1])
+                {
+                    int sum = (int)leftSums[nums.Length - 1];
+
+                    ans = target / sum * nums.Length;
+                    target = target % sum;
+                }
+
+                int rem = nums.Length;
+                int left = 0, right = 0;
+                while (left < leftSums.Length)
+                {
+                    while (right < leftSums.Length && leftSums[right] - leftSums[left] < target)
+                    {
+                        right++;
+                    }
+
+                    if (right == leftSums.Length)
+                    {
+                        break;
+                    }
+
+                    while (left < leftSums.Length && leftSums[right] - leftSums[left] > target)
+                    {
+                        left++;
+                    }
+
+                    if (leftSums[right] - leftSums[left] == target)
+                    {
+                        rem = Math.Min(rem, right - left);
+                        left++;
+                    }
+                }
+
+                if (rem == nums.Length)
+                {
+                    return -1;
+                }
+
+                return ans + rem;
+            }
+        }
+    }
+}
---
+++ LeetCode/WeeklyContest/Weekly365/Problem2876.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.WeeklyContest.Weekly365
+{
+    using Shared.Utils;
+
+    internal class Problem2876
+    {
+        public class Solution
+        {
+            public int[] CountVisitedNodes(IList<int> edges)
+            {
+                int[] ans = ArrayUtils.MakeArray(edges.Count, -1);
+                for (int i = 0; i < edges.Count; i++)
+                {
+                    if (ans[i] == -1)
+                    {
+                        List<int> visited = new List<int>();
+                        int now = i;
+                        while (!visited.Contains(now) && ans[now] == -1)
+                        {
+                            ans[now] = 0;
+                            visited.Add(now);
+                            now = edges[now];
+                        }
+
+                        bool newCircle = false;
+                        if (ans[now] == 0)
+                        {
+                            newCircle = true;
+
+                            // Find a circle starting from now
+                            // record the circle length on all nodes in the circle
+                            int nowIndex = visited.IndexOf(now);
+                            int circleCnt = visited.Count - nowIndex;
+                            for (int j = nowIndex; j < visited.Count; ++j)
+                            {
+                                // all nodes in the circle, set the result
+                                ans[visited[j]] = circleCnt;
+                            }
+                        }
+
+                        if (ans[now] != -1)
+                        {
+                            // find a path to a node which already visited
+                            int endIndex = newCircle ? visited.IndexOf(now) : visited.Count;
+                            for (int j = 0; j < endIndex; j++)
+                            {
+                                ans[visited[j]] = endIndex - j + ans[now];
+                            }
+                        }
+                    }
+                }
+
+                return ans;
+            }
+        }
+    }
+}
