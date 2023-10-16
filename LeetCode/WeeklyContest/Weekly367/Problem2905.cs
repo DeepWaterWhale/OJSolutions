eef848321eb845f1ea728bd7b66afe02d267d2a7
xiangjli <xiangjun.app@hotmail.com>
2023-10-16 23:15:27 +0800

    Solve weekly 367

---
+++ LeetCode/WeeklyContest/Weekly367/Problem2905.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.WeeklyContest.Weekly367
+{
+    internal class Problem2905
+    {
+        public class Solution
+        {
+            public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+            {
+                var ans = this.FindIndices(nums, indexDifference, valueDifference, true);
+                if (ans[0] == -1)
+                {
+                    ans = this.FindIndices(nums, indexDifference, valueDifference, false);
+                }
+
+                return ans;
+            }
+
+            public int[] FindIndices(int[] nums, int indexDifference, int valueDifference, bool inc)
+            {
+                int idx = 0;
+                List<int> queue = new List<int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (queue.Count == 0 || (nums[i] > nums[queue.Last()]) == inc)
+                    {
+                        queue.Add(i);
+                    }
+
+                    while (idx < queue.Count && i - queue[idx] >= indexDifference)
+                    {
+                        idx++;
+                    }
+
+                    if (idx != 0 && Math.Abs(nums[i] - nums[queue[idx - 1]]) >= valueDifference)
+                    {
+                        return new int[] { queue[idx - 1], i };
+                    }
+                }
+
+                return new int[] { -1, -1 };
+            }
+        }
+    }
+}
---
+++ LeetCode/WeeklyContest/Weekly367/Problem2906.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.WeeklyContest.Weekly367
+{
+    using Shared.DataStructures.SegmentTree;
+
+    internal class Problem2906
+    {
+        public class Solution
+        {
+            public int[][] ConstructProductMatrix(int[][] grid)
+            {
+                int height = grid.Length;
+                int width = grid[0].Length;
+                var flat = new List<int>(height * width);
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        flat.Add(grid[i][j] % 12345);
+                    }
+                }
+
+                var st = new ReadOnlySegmentTree<int, int>(
+                    elements: flat,
+                    calFunc: num => num,
+                    mergeFunc: (a, b) => a * b % 12345);
+
+                int index = 0;
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (index == 0)
+                        {
+                            grid[i][j] = st.Query(index + 1, flat.Count - 1);
+                        }
+                        else if (index == flat.Count - 1)
+                        {
+                            grid[i][j] = st.Query(0, index - 1);
+                        }
+                        else
+                        {
+                            grid[i][j] = st.Query(0, index - 1) * st.Query(index + 1, flat.Count - 1) % 12345;
+                        }
+
+                        index++;
+                    }
+                }
+
+                return grid;
+            }
+        }
+    }
+}
