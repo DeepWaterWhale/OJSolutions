9f3afaed6b92097aacb99a85b4bd90d7c3ad17d7
xiangjli <xiangjun.app@hotmail.com>
2023-10-17 09:01:53 +0800

    Solve weekly 353

---
+++ LeetCode/WeeklyContest/Weekly353/Problem2771.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.WeeklyContest.Weekly353
+{
+    internal class Problem2771
+    {
+        public class Solution
+        {
+            public int MaxNonDecreasingLength(int[] nums1, int[] nums2)
+            {
+                int[,] dp = new int[nums1.Length, 2];
+                dp[nums1.Length - 1, 0] = 1;
+                dp[nums1.Length - 1, 1] = 1;
+
+                int ans = 1;
+                for (int i = nums1.Length - 2; i >= 0; i--)
+                {
+                    dp[i, 0] = 1;
+                    if (nums1[i] <= nums1[i + 1])
+                    {
+                        dp[i, 0] = Math.Max(dp[i, 0], dp[i + 1, 0] + 1);
+                    }
+
+                    if (nums1[i] <= nums2[i + 1])
+                    {
+                        dp[i, 0] = Math.Max(dp[i, 0], dp[i + 1, 1] + 1);
+                    }
+
+                    ans = Math.Max(ans, dp[i, 0]);
+
+                    dp[i, 1] = 1;
+                    if (nums2[i] <= nums1[i + 1])
+                    {
+                        dp[i, 1] = Math.Max(dp[i, 1], dp[i + 1, 0] + 1);
+                    }
+
+                    if (nums2[i] <= nums2[i + 1])
+                    {
+                        dp[i, 1] = Math.Max(dp[i, 1], dp[i + 1, 1] + 1);
+                    }
+
+                    ans = Math.Max(ans, dp[i, 1]);
+                }
+
+                return ans;
+            }
+        }
+    }
+}
---
+++ LeetCode/WeeklyContest/Weekly353/Problem2772.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.WeeklyContest.Weekly353
+{
+    using Shared.Utils;
+
+    internal class Problem2772
+    {
+        public class Solution
+        {
+            public bool CheckArray(int[] nums, int k)
+            {
+                // op is the number we need to minus at current num
+                int op = 0;
+                // ops[i] = the count the operation starting from index i
+                int[] ops = ArrayUtils.MakeArray(nums.Length, 0);
+                for (int i = 0; i < nums.Length; ++i)
+                {
+                    if (i >= k)
+                    {
+                        op -= ops[i - k];
+                    }
+
+                    if (nums[i] < op)
+                    {
+                        return false;
+                    }
+
+                    ops[i] = nums[i] - op;
+                    op = nums[i];
+                    // Console.WriteLine($"num = {nums[i]}, ops[i] = {ops[i]}, op = {op}");
+                }
+
+                return op == ops[nums.Length - k];
+            }
+        }
+    }
+}
