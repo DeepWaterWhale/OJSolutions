0465c82e32dc7e4f5cad645f380b8d43beb79120
xiangjli <xiangjun.app@hotmail.com>
2023-11-05 11:51:16 +0800

    Update weekly 366

---
+++ LeetCode/WeeklyContest/Weekly366/Problem2896.cs
@@ -0,0 +1,83 @@
+﻿namespace LeetCode.TODOs.Weekly366
+{
+    using Shared.Utils;
+
+    internal class Problem2896
+    {
+        public class Solution
+        {
+            public int MinOperations(string s1, string s2, int x)
+            {
+                // -2 means not initialized, -1 means not possible
+                var dp = ArrayUtils.Make2DArray(s1.Length, 4, -2);
+                int length = s1.Length;
+                if (s1[length - 1] == s2[length - 1])
+                {
+                    dp[length - 1, 0] = 0;
+                    dp[length - 1, 1] = 250000;
+                    dp[length - 1, 2] = 250000;
+                    dp[length - 1, 3] = 0;
+                }
+                else
+                {
+                    dp[length - 1, 0] = 250000;
+                    dp[length - 1, 1] = 0;
+                    dp[length - 1, 2] = 0;
+                    dp[length - 1, 3] = 250000;
+                }
+
+                int ans = this.Solve(s1, s2, x, dp, 0, 0);
+                if (ans >= 250000)
+                {
+                    return -1;
+                }
+
+                return ans;
+            }
+
+            // dp status including:
+            //     1. Has unpaired FlipAny
+            //     1. Current char is flipped or not by FlipNext
+            // dp[k][0] = Not Flipped by FlipNext   &&   No unpaired FlipAny
+            // dp[k][1] = Not Flipped by FlipNext   &&   Has unpaired FlipAny
+            // dp[k][2] = Flipped by FlipNext       &&   No unpaired FlipAny
+            // dp[k][3] = Flipped by FlipNext       &&   Has unpaired FlipAny
+            // Need to calculate dp[0][0]
+            private int Solve(string s1, string s2, int x, int[,] dp, int index, int status)
+            {
+                if (dp[index, status] != -2)
+                {
+                    return dp[index, status];
+                }
+
+                int ans = 250000;
+                if ((s1[index] == s2[index]) == (status < 2))
+                {
+                    // Current s1[index] == s2[index], can not 
+                    // Case 1: Do nothing:
+                    ans = Math.Min(ans, this.Solve(s1, s2, x, dp, index + 1, status % 2));
+                }
+                else
+                {
+                    // Case 2: flip current with flip any
+                    if (status % 2 == 0)
+                    {
+                        // No unpaired flip any
+                        ans = Math.Min(ans, this.Solve(s1, s2, x, dp, index + 1, 1) + x);
+                    }
+                    else
+                    {
+                        // Has unpaired flip any
+                        ans = Math.Min(ans, this.Solve(s1, s2, x, dp, index + 1, 0));
+                    }
+
+                    // Case 3: flip current with next
+                    ans = Math.Min(ans, this.Solve(s1, s2, x, dp, index + 1, 2 + status % 2) + 1);
+                }
+
+                dp[index, status] = ans;
+                return ans;
+            }
+        }
+    }
+}
---
+++ LeetCode/WeeklyContest/Weekly366/Problem2897.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.TODOs.Weekly366
+{
+    using Shared;
+    using Shared.Utils;
+
+    internal class Problem2897
+    {
+        public class Solution
+        {
+            public int MaxSum(IList<int> nums, int k)
+            {
+                int[] cnts = ArrayUtils.MakeArray(32, 0);
+                foreach (int num in nums)
+                {
+                    for (int i = 0; i < 32; ++i)
+                    {
+                        if ((num & (1 << i)) != 0)
+                        {
+                            cnts[i]++;
+                        }
+                    }
+                }
+
+                long ans = 0;
+                while (k-- > 0)
+                {
+                    long num = 0;
+                    for (int i = 0; i < 32; ++i)
+                    {
+                        if (cnts[i] > 0)
+                        {
+                            num += 1 << i;
+                            cnts[i]--;
+                        }
+                    }
+
+                    if (num == 0)
+                    {
+                        break;
+                    }
+
+                    ans = (ans + (num * num % Constants.MOD)) % Constants.MOD;
+                }
+
+                return (int)ans;
+            }
+        }
+    }
+}
